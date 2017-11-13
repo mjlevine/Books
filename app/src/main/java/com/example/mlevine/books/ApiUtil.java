40737448d4d137d7fbb4bb77745c3d0e361c1db5
Marc Levine <marc.levine@market6.com>
2017-11-13 09:27:07 -0600

    ability to display hard-coded list of books prettily

--- app/src/main/java/com/example/mlevine/books/ApiUtil.java
+++ app/src/main/java/com/example/mlevine/books/ApiUtil.java
@@ -3,10 +3,14 @@ package com.example.mlevine.books;
 import android.net.Uri;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ApiUtil {
@@ -56,5 +60,49 @@ public class ApiUtil {
             connection.disconnect();
         }
 
+    }
+
+    public static ArrayList<Book> getBooksFromJson(String json) {
+        final String ID = "id";
+        final String TITLE = "title";
+        final String SUBTITLE = "subtitle";
+        final String AUTHORS = "authors";
+        final String PUBLISHER = "publisher";
+        final String PUBLISHED_DATE = "publishedDate";
+        final String ITEMS = "items";
+        final String VOLUME_INFO = "volumeInfo";
+
+        ArrayList<Book> books = new ArrayList<Book>();
+        try {
+            JSONObject jsonBooks = new JSONObject(json);
+            JSONArray arrayBooks = jsonBooks.getJSONArray(ITEMS);
+            for (int i = 0; i < arrayBooks.length(); i++) {
+                JSONObject bookJson = arrayBooks.getJSONObject(i);
+                JSONObject volumeInfoJson = bookJson.getJSONObject(VOLUME_INFO);
+
+                if (volumeInfoJson.isNull(AUTHORS)) continue;
+                int numberOfAuthors = volumeInfoJson.getJSONArray(AUTHORS).length();
+                String[] authors = new String[numberOfAuthors];
+                for (int j = 0; j < numberOfAuthors; j++) {
+                    authors[j] = volumeInfoJson.getJSONArray(AUTHORS).get(j).toString();
+                }
+
+                Book book = new Book(
+                        bookJson.getString(ID),
+                        volumeInfoJson.getString(TITLE),
+                        volumeInfoJson.isNull(SUBTITLE) ? "" : volumeInfoJson.getString(SUBTITLE),
+                        authors,
+                        volumeInfoJson.isNull(PUBLISHER) ? "" : volumeInfoJson.getString(PUBLISHER),
+                        volumeInfoJson.getString(PUBLISHED_DATE)
+                );
+                books.add(book);
+            }
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return books;
     }
 }
--- app/src/main/java/com/example/mlevine/books/BookListActivity.java
+++ app/src/main/java/com/example/mlevine/books/BookListActivity.java
@@ -3,6 +3,8 @@ package com.example.mlevine.books;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
@@ -11,15 +13,21 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 import java.net.URL;
+import java.util.ArrayList;
 
 public class BookListActivity extends AppCompatActivity {
     private ProgressBar mLoadingProgress;
+    private RecyclerView rvBooks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
         mLoadingProgress = (ProgressBar) findViewById(R.id.pb_loading);
+        rvBooks = (RecyclerView) findViewById(R.id.rv_books);
+        LinearLayoutManager booksLayoutManager = new LinearLayoutManager(
+                this, LinearLayoutManager.VERTICAL, false);
+        rvBooks.setLayoutManager(booksLayoutManager);
 
         try {
             URL bookUrl = ApiUtil.buildUrl("cooking");
@@ -49,13 +57,16 @@ public class BookListActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String result) {
-            TextView tvResult = (TextView) findViewById(R.id.tvResponse);
             TextView tvError = (TextView) findViewById(R.id.tv_error);
 
             tvError.setVisibility(result == null ? View.VISIBLE : View.INVISIBLE);
-            tvResult.setVisibility(result == null ? View.INVISIBLE : View.VISIBLE);
-            tvResult.setText(result);
+            rvBooks.setVisibility(result == null ? View.INVISIBLE : View.VISIBLE);
             mLoadingProgress.setVisibility(View.INVISIBLE);
+
+            ArrayList<Book> books = ApiUtil.getBooksFromJson(result);
+
+            BooksAdapter adapter = new BooksAdapter(books);
+            rvBooks.setAdapter(adapter);
         }
 
         @Override
